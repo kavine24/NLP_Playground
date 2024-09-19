17fb2bc357c21e139f6e56e75c5b0c201336f28e
kavine24 <kavin.elamurugan@gmail.com>
2024-09-18 22:04:04 -0400

    finished database linking and setup (for real this time) (i think)

---
+++ server/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+conda run -n server python manage.py makemigrations
+conda run -n server python manage.py migrate
+conda run -n server python manage.py init_admin --no-input
+conda run -n server python manage.py runserver 0.0.0.0:8000
+
