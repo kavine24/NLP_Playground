4f55ad921c70596107c64eeb2ef7c8f3d4edafd0
kavine24 <kavin.elamurugan@gmail.com>
2024-09-23 13:20:22 -0400

    starting front end

--- server/chatbox/views.py
+++ server/chatbox/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render,  HttpResponse
+from chatbox.models import Documents
+import ollama
+
+import sys
 
 # Create your views here.
 def chat(request):
     return HttpResponse("Hello chat")
 
 def doc_viewer(request):
-    return HttpResponse("Hello doc viewer")
+    response = ollama.chat(model='llama3', messages=[
+    {
+        'role': 'user',
+        'content': 'Why is the sky blue?',
+    },
+    ])
+
+    return HttpResponse(response['message']['content'])
+    docs = Documents.objects.all()
+
+    return render(request, 'doc_viewer.html')
 
 def doc_upload(request):
     return HttpResponse("Hello doc viewer")
--- server/start.sh
+++ server/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+conda run -n server ollama serve & \
 
-conda run -n server python manage.py makemigrations
-conda run -n server python manage.py migrate
-conda run -n server python manage.py init_admin --no-input
+conda run -n server python manage.py makemigrations && \
+conda run -n server python manage.py migrate && \
+conda run -n server python manage.py init_admin --no-input && \
 conda run -n server python manage.py runserver 0.0.0.0:8000
 
